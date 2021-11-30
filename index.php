0d2179c3545fcf9a4815be2ab4738b5ef1bc4e3e
nyatsukichi <95033524+nyatsukichi@users.noreply.github.com>
2021-11-30 22:43:54 +0900

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="stylesheet.css">
+    <meta charset="utf-8">
+    <title>NIKUKYU CO.,LTD</title>
+  </head>
+  <body>
+    <h1 class="title">NIKUKYU CO.,LTD</h1>
+    <p>肉球で世界を平和に</p>
+  </body>
+</html>
